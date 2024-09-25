18d4d50672bc8c82b3b098732e3adeaa1f925d52
amar-eng <am24mo@gmail.com>
2024-09-25 04:04:49 -0400

    changes done

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,10 @@ export default function Home() {
   return (
     <div className="m-4">
       <Navbar />
+      <div className="">This is my hero section</div>
+      <div className="">This is section for Projects and Portfolio</div>
+      <div className="">Contact and About Me Section</div>
+      <footer>This is a footer</footer>
     </div>
   );
 }
